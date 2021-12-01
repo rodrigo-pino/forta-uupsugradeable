563da6d425272d2c95c42cee3a12907b99b270ea
RodroVMS <democlesvanshepars@gmail.com>
2021-12-01 10:18:31 -0500

    delete unused abi

--- src/constants.ts
+++ src/constants.ts
@@ -1,6 +1,2 @@
 export const UPGRADED_EVENT_ABI =
   "event Upgraded(address indexed implementation)";
-export const UPGRADE_TO_ABI =
-  "function upgradeTo(address newImplementation) external virtual onlyProxy";
-export const UPGRADE_TO_AND_CALL_ABI =
-  "function upgradeToAndCall(address newImplementation, bytes memory data) external payable virtual onlyProxy";
